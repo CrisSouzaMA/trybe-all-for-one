7408d9262729ccd1980925821cd1c229b71a012c
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-06-10 10:50:52 -0300

    Feat: implementa query para atender requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,3 @@
+/* 8 - Faça uma consulta que retorne três colunas, respectivamente, com os nomes 'A', 'Trybe' e 'eh', e com valores referentes a soma de '5 + 6', a string 'de', a soma de '2 + 8'. Resposta: */
+
+SELECT (5+6) as A, 'de' as Trybe, (2+8) as eh;
